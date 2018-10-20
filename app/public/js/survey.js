df3d0bc1d554a56a1d99c734ca70f6286d58deea
Tinedanzer <johnhillallen@gmail.com>
2018-10-20 00:34:21 -0500

    last few updates

--- app/public/js/survey.js
+++ app/public/js/survey.js
@@ -1,9 +1,26 @@
-// $(document).ready(function(){
 // $("#magic").on("click", function (event) {
 //     event.preventDefault();
 //     console.log("button clicked");
+//     // validation function
+//     function validateForm() {
+//         var isValid = true;
+//         $(".form-control").each(function() {
+//           if ($(this).val() === "") {
+//             isValid = false;
+//           }
+//         });
 
-//     const friendsMag = {
+//         $(".chosen-select").each(function() {
+
+//           if ($(this).val() === "") {
+//             isValid = false;
+//           }
+//         });
+//         return isValid;
+//       }
+//     //   if fields are filled out  then here we go.
+//     if(validateForm()){
+//         const friendsMag = {
 //       name: $("#name").val(),
 //       photo: $("#photo").val(),
 //       scores: [
@@ -20,19 +37,25 @@
 //       ]
 //     };
 //     let currentURL =window.location.origin;
+//     // friendsMag is the request happening; and function(data) is the response we get back
 //     $.post(currentURL + '/api/friends', friendsMag, function (data){
-//         console.log(data);
-//         console.log(friendsMag);
+        // console.log(data);
+        // console.log(friendsMag);
         
-//         $("#match").text(data.name);
-//         $("#matchImg").attr("src", data.photo);
-//         // $("#results-modal").modal("toggle");
+        // pulls the data from apiRoutes to give us a modal popup of our match
+        $("#match").text(data.match);
+        $("#matchImg").attr("src", data.matchImage);
+        $("#results-modal").modal("toggle");
 
-//         // clears input fields
-//         $("#name").val("");
-//         $("photo").val("");
-//         $(".chosen-select").val("");
-//     })
-//     return false;
-// });
-// })
+        // clears input fields
+        $("#name").val("");
+        $("#photo").val("");
+        $(".chosen-select").val("");
+    })
+    // closing If statement with }
+    }
+else{
+    alert("Fill out all of the fields please, this is very important to ensure we match you with a great friend! :-) (choose all 3s for best results ;-) )");
+}
+    // return false;
+})
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -6,28 +6,43 @@ module.exports = function(app){
         res.json(ultFriends)
     });
 
-    // Create New Characters - takes in JSON input
-    app.post("/api/friends", function(req, res) {
+    // Create Friends - takes in JSON input
+    // the  request is from the  ajax request, submitting the newFriend; the response is what we 
+    // give back to the front end.
+    app.post("/api/friends", function(request, response) {
     // req.body hosts is equal to the JSON post sent from the user
     // This works because of our body-parser and express middleware
-    let newFriend = req.body;
-    // let match ='';
-    // let matchImage='';
-
+    let newFriend = request.body;
+    
     // Using a RegEx Pattern to remove spaces from newFriend
-    // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
+    // You can read more about RegEx Patterns at https://www.regexbuddy.com/regex.html
     newFriend.name = newFriend.name.replace(/\s+/g, "").toLowerCase();
     console.log(newFriend); 
-
+// pushes the values submitted to the friends.js file to save a new friend!
     ultFriends.push(newFriend);
     // res.json(newFriend);
-    // to decide who  your match is, and then displays the results via res.json(match).
-    for (let i = 0; i < ultFriends.length; i++) {
-        const element = array[i];
-        
+    // to decide who  your match is, and then displays the results via res.json.
+       //matching friends
+       let match = "";
+       let matchImage = "";
+       let total = 100;
+   
+       for (let i = 0; i < ultFriends.length; i++) {
     
-    res.json(match);
-    }
-    // res.json({match:match, matchImage:matchImage});
+         let diff = 0;
+   
+         for (let x in newFriend.scores) {
+           let currentInfo = parseInt(ultFriends[i].scores);
+           let newInfo = parseInt(newFriend.scores[x]);
+           diff = Math.abs(currentInfo - newInfo);
+         };
+         if (diff < total) {
+           total = diff;
+           match = ultFriends[i].name;
+           matchImage = ultFriends[i].photo;
+         }
+       }
+       //returning response
+       response.json({ match: match, matchImage: matchImage });
   });
 }
